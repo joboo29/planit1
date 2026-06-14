a683547a27364c3f297eea53e46bd13892684a35
Jonathan Boo <43100152+joboo29@users.noreply.github.com>
2026-06-14 10:29:38 -0400

    Added Comment

--- planit1/src/Main.java
+++ planit1/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
+    //Add comment
     public static void main(String[] args) {
         ReportGenerator rg = new ReportGenerator();
         rg.GenerateReport("Call Center 1");
